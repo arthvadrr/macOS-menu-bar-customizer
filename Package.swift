07dbf9eab7933fb1cf944bf0e77db808610acf56
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-06-15 00:36:07 -0500

    Working on menu settings

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Menu Bar Customizer",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .executable(
+            name: "Menu Bar Customizer", 
+            targets: ["Menu Bar Customizer"]
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "Menu Bar Customizer",
+            dependencies: [],
+            path: "Sources"
+        )
+    ]
+);
